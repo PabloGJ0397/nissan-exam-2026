de1766b8023c2837f539e53abd4cc44720a2918f
GJP <garciajuarezpablo97@gmail.com>
2026-03-05 18:29:14 +0100

    chore: Test finAll in Main with harcoded data in VehicleMemLocalDataSource

---
+++ src/dev/pablogj0397/nissan/vehicle/MainVehicles.java
@@ -0,0 +1,9 @@
+package dev.pablogj0397.nissan.vehicle;
+
+import dev.pablogj0397.nissan.vehicle.presentation.VehicleView;
+
+public class MainVehicles {
+    public static void main(String[] args) {
+        VehicleView.findAll();
+    }
+}
