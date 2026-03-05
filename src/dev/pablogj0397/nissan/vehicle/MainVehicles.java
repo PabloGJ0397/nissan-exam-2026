9b7e9afa85bc4d305c3b093118423547ca72f4c5
GJP <garciajuarezpablo97@gmail.com>
2026-03-05 19:09:22 +0100

    feat(chore): Test the new feature on main Closes: #3

--- src/dev/pablogj0397/nissan/vehicle/MainVehicles.java
+++ src/dev/pablogj0397/nissan/vehicle/MainVehicles.java
@@ -4,6 +4,12 @@ import dev.pablogj0397.nissan.vehicle.presentation.VehicleView;
 
 public class MainVehicles {
     public static void main(String[] args) {
+        //TEST FEATURE GetVehicles = findAll
+        // VehicleView.findAll();
+
+        //TEST FEATURE SaveVehicles
+        VehicleView.findAll();
+        VehicleView.saveVehicle();
         VehicleView.findAll();
     }
 }
